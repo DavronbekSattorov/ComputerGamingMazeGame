98896c07a52889200347dde6815a88fc430b42e1
Davronbek <davsattor@gmail.com>
2021-01-25 01:09:23 +0100

    Maze Game

---
+++ Lab3/Assets/CameraSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitch : MonoBehaviour
+{
+
+    [SerializeField] GameObject MainCam;
+    [SerializeField] GameObject FirstPersonCam;
+    // Use this for initialization
+    void Start()
+    {
+
+        MainCam.SetActive(false);
+        FirstPersonCam.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            MainCam.SetActive(!MainCam.activeSelf);
+            FirstPersonCam.SetActive(!FirstPersonCam.activeSelf);
+
+        }
+
+    }
+}
---
+++ Lab3/Assets/PlayerCollision.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerCollision : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI winLabel;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject other = collision.gameObject;
+        if (other.CompareTag("ExitDoor"))
+        {
+            winLabel.gameObject.SetActive(true);
+        }
+
+    }
+
+
+
+}
